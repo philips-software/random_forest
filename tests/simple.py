81cdba757dcf40cd1e46ef2857fe2b229b33e6f6
Mark Spanbroek <mark@spanbroek.net>
2019-10-01 12:27:58 +0200

    First MPyC test

--- tests/simple.py
+++ tests/simple.py
@@ -1,7 +1,16 @@
 import unittest
+from mpyc.runtime import mpc
+
+secint = mpc.SecInt()
+input = mpc.input
+output = mpc.output
+run = mpc.run
 
 
 class SimpleTest(unittest.TestCase):
 
-    def test_fails(self):
-        self.assertEqual(1, 2)
+    def test_mpc(self):
+        a, *_ = input(secint(40))
+        b, *_ = input(secint(2))
+        c = run(output(a + b))
+        self.assertEqual(c, 42)
