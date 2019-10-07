77b168803e794c5a23046c19731549a51512191f
Mark Spanbroek <mark@spanbroek.net>
2019-10-07 13:06:59 +0200

    Calculate correct Gini impurity

--- src/gini.py
+++ src/gini.py
@@ -1,2 +1,8 @@
-def gini_impurity(matrix):
-    return 1
+def gini_impurity(amount_classified_as_zero, amount_classified_as_one):
+    if amount_classified_as_one + amount_classified_as_zero == 0:
+        return 1
+
+    total = amount_classified_as_zero + amount_classified_as_one
+    ratio_left = amount_classified_as_zero / total
+    ratio_right = amount_classified_as_one / total
+    return 1 - ratio_left ** 2 - ratio_right ** 2
--- tests/gini.py
+++ tests/gini.py
@@ -4,5 +4,12 @@ from src.gini import gini_impurity
 
 class GiniTest(unittest.TestCase):
 
-    def test_empty_matrix(self):
-        self.assertEqual(gini_impurity([]), 1)
+    def test_empty_attribute(self):
+        self.assertEqual(gini_impurity(0, 0), 1)
+
+    def test_balanced_attribute(self):
+        self.assertEqual(gini_impurity(3, 3), 0.5)
+
+    def test_homogeneous_attribute(self):
+        self.assertEqual(gini_impurity(3, 0), 0)
+        self.assertEqual(gini_impurity(0, 3), 0)
