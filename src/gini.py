faad587c5cbcd1a832e77eac0e9cc5a1b8f7eeda
Mark Spanbroek <mark@spanbroek.net>
2019-10-08 09:39:33 +0200

    Calculate gini gain (one failing test)

--- src/gini.py
+++ src/gini.py
@@ -3,6 +3,44 @@ def gini_impurity(amount_classified_as_zero, amount_classified_as_one):
         return 1
 
     total = amount_classified_as_zero + amount_classified_as_one
-    ratio_left = amount_classified_as_zero / total
-    ratio_right = amount_classified_as_one / total
-    return 1 - ratio_left ** 2 - ratio_right ** 2
+    ratio_zero = amount_classified_as_zero / total
+    ratio_one = amount_classified_as_one / total
+    return 1 - ratio_zero ** 2 - ratio_one ** 2
+
+
+def gini_gain_scaled_quotient(
+        left_total,
+        right_total,
+        left_amount_classified_zero,
+        left_amount_classified_one,
+        right_amount_classified_zero,
+        right_amount_classified_one):
+    """
+    Returns a pair of numbers that represent the Gini gain, given a split in
+    the dataset.
+
+    Keyword arguments:
+    left_total -- the number of samples that on the left side of the split
+    right_total -- the number of samples that on the right side of the split
+    left_amount_classified_zero -- the number of samples on the left side that are classified as '0'
+    left_amount_classified_one -- the number of samples on the left side that are classified as '1'
+    right_amount_classified_zero -- the number of samples on the right side that are classified as '0'
+    right_amount_classified_one -- the number of samples on the right side that are classified as '1'
+
+    Return value:
+    (numerator, denominator) -- such that the Gini gain equals
+    (1 / total) * (numerator / denominator), where total is the total number
+    of samples (left and right)
+
+    See also:
+    Explanation of Gini gain -- https://victorzhou.com/blog/gini-impurity/
+    Secure Training of Decision Trees with Continuous Attributes -- paper to be
+    published by Mark Abspoel, Daniel Escudero and Nikolaj Volgushev
+    """
+    numerator = \
+        right_total * (left_amount_classified_zero ** 2 +
+                       left_amount_classified_one ** 2) + \
+        left_total * (right_amount_classified_zero ** 2 +
+                      right_amount_classified_one ** 2)
+    denominator = left_total * right_total
+    return (numerator, denominator)
--- tests/gini.py
+++ tests/gini.py
@@ -1,5 +1,5 @@
 import unittest
-from src.gini import gini_impurity
+from src.gini import *
 
 
 class GiniTest(unittest.TestCase):
@@ -13,3 +13,21 @@ class GiniTest(unittest.TestCase):
     def test_homogeneous_attribute(self):
         self.assertEqual(gini_impurity(3, 0), 0)
         self.assertEqual(gini_impurity(0, 3), 0)
+
+    def test_gini_gain_single_value(self):
+        numerator, denominator = gini_gain_scaled_quotient(1, 0, 1, 0, 0, 0)
+        total = 1
+        gain = (1 / total) * numerator / denominator
+        self.assertEqual(gain, 1)
+
+    def test_gini_gain_uniform(self):
+        numerator, denominator = gini_gain_scaled_quotient(2, 2, 1, 1, 1, 1)
+        total = 4
+        gain = (1 / total) * (numerator / denominator)
+        self.assertEqual(gain, 0.5)
+
+    def test_gini_gain_perfect_split(self):
+        numerator, denominator = gini_gain_scaled_quotient(2, 2, 2, 0, 0, 2)
+        total = 4
+        gain = (1 / total) * (numerator / denominator)
+        self.assertEqual(gain, 1)
