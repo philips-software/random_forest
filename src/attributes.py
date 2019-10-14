c6a82aafe7608f5171e33499a7d01246dd21cb08
Mark Spanbroek <mark@spanbroek.net>
2019-10-14 11:20:08 +0200

    Partition a partial dataset

--- src/attributes.py
+++ src/attributes.py
@@ -54,7 +54,14 @@ def partition_on(samples, is_active, attribute_index, threshold):
     left = [value <= threshold for value in selected_attribute]
     right = [(1 - l) for l in left]
 
+    left = zero_if_inactive(left, is_active)
+    right = zero_if_inactive(right, is_active)
+
     return left, right
+
+
+def zero_if_inactive(values, is_active):
+    return mpc.schur_prod(values, is_active)
 
 
 def get_column(samples, attribute_index):
--- tests/attributes.py
+++ tests/attributes.py
@@ -37,10 +37,24 @@ class AttributeTests(unittest.TestCase):
             [s(1), s(0), s(1), s(1)],
             [s(0), s(0), s(0), s(1)],
         ]
+        is_active = [s(1), s(1), s(1)]
         best_attribute = s(2)
         best_attribute_value = s(0)
-        is_active = [s(1), s(1), s(1)]
         left_active, right_active = partition_on(
             samples, is_active, best_attribute, best_attribute_value)
         self.assertEqual(run(output(left_active)), [0, 0, 1])
         self.assertEqual(run(output(right_active)), [1, 1, 0])
+
+    def test_partition_on_partial_dataset(self):
+        samples = [
+            [s(0), s(1), s(1), s(0)],
+            [s(1), s(0), s(1), s(1)],
+            [s(0), s(0), s(0), s(1)],
+        ]
+        is_active = [s(1), s(0), s(0)]
+        best_attribute = s(2)
+        best_attribute_value = s(0)
+        left_active, right_active = partition_on(
+            samples, is_active, best_attribute, best_attribute_value)
+        self.assertEqual(run(output(left_active)), [0, 0, 0])
+        self.assertEqual(run(output(right_active)), [1, 0, 0])
