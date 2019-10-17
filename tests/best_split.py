3073aadc91f101aeecc34d58888506569194dcc0
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-10-17 10:25:54 +0200

    Use dataset instead of list of samples for selecting best attribute

--- tests/best_split.py
+++ tests/best_split.py
@@ -1,5 +1,6 @@
 import unittest
 from mpyc.runtime import mpc
+from src.dataset import ObliviousDataset
 from src.best_split import select_best_attribute
 
 s = mpc.SecInt()
@@ -9,24 +10,25 @@ run = mpc.run
 
 class AttributeTests(unittest.TestCase):
     def test_select_best_attribute(self):
-        samples = [
+        samples = ObliviousDataset(
             [s(0), s(1), s(1), s(0)],
             [s(1), s(0), s(1), s(1)],
             [s(0), s(0), s(0), s(1)],
-        ]
+        )
         outcomes = [s(1), s(1), s(0)]
         best_attribute = select_best_attribute(samples, outcomes)
         self.assertEqual(run(output(best_attribute)), 2)
 
     def test_select_best_attribute_no_samples(self):
-        self.assertRaises(ValueError, select_best_attribute, [], [])
+        samples = ObliviousDataset()
+        self.assertRaises(ValueError, select_best_attribute, samples, [])
 
     def test_select_best_attribute_with_gini_denominator_zero(self):
-        samples = [
+        samples = ObliviousDataset(
             [s(0), s(0), s(1), s(0)],
             [s(0), s(0), s(1), s(0)],
             [s(0), s(0), s(0), s(0)],
-        ]
+        )
         outcomes = [s(1), s(1), s(0)]
         best_attribute = select_best_attribute(samples, outcomes)
         self.assertEqual(run(output(best_attribute)), 2)
