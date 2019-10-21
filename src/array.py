1f7f1a950c93ed210e90ffae3c2f154d1360a25d
Mark Spanbroek <mark@spanbroek.net>
2019-10-21 17:31:29 +0200

    Add ObliviousArray.select() to obliviously select elements of an array

--- src/array.py
+++ src/array.py
@@ -1,15 +1,33 @@
 from dataclasses import dataclass
 from typing import Any
 from mpyc.runtime import mpc
+from src.output import Secret, output
 
 
 @dataclass
-class ObliviousArray:
+class ObliviousArray(Secret):
     values: [Any]
+    included: [Any]
 
-    def __init__(self, *values):
+    def __init__(self, *values, included=None):
         self.values = values
+        self.included = included
+
+    def select(self, *include):
+        if self.included == None:
+            included = include
+        else:
+            included = mpc.schur_prod(list(self.included), include)
+        return ObliviousArray(*self.values, included=included)
 
     def __getitem__(self, index):
         is_selected = [i == index for i in range(len(self.values))]
         return mpc.matrix_prod([is_selected], [self.values], True)[0][0]
+
+    async def output(self):
+        values = [await output(value) for value in self.values]
+        if self.included:
+            included = await output(self.included)
+        else:
+            included = [True] * len(self.values)
+        return [values[i] for i in range(len(values)) if included[i]]
--- tests/array.py
+++ tests/array.py
@@ -1,6 +1,7 @@
 import unittest
 from src.array import ObliviousArray
 from mpyc.runtime import mpc
+from src.output import output
 
 s = mpc.SecInt()
 
@@ -12,6 +13,21 @@ class ObliviousArrayTest(unittest.TestCase):
         self.assertEquals(reveal(array[s(1)]), 20)
         self.assertEquals(reveal(array[s(2)]), 30)
 
+    def test_reveal_all_elements(self):
+        array = ObliviousArray(s(10), s(20), s(30))
+        self.assertEqual(reveal(array), [10, 20, 30])
+
+    def test_reveal_selected_elements(self):
+        array = ObliviousArray(s(10), s(20), s(30))
+        array = array.select(s(0), s(1), s(0))
+        self.assertEqual(reveal(array), [20])
+
+    def test_select_and_select_again(self):
+        array = ObliviousArray(s(10), s(20), s(30))
+        array = array.select(s(1), s(0), s(1))
+        array = array.select(s(0), s(1), s(1))
+        self.assertEqual(reveal(array), [30])
+
 
 def reveal(secret):
-    return mpc.run(mpc.output(secret))
+    return mpc.run(output(secret))
