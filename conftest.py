9d8d4c02e38cb871ae5daaec751850d14da6de28
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-10-22 09:48:23 +0200

    Switches to pytest for unit testing
    You can now run `pytest` for a single run, or `ptw` for watching tests.

---
+++ conftest.py
@@ -0,0 +1,3 @@
+# This file needs to be present for pytest to figure out the correct
+# PYTHONROOT while running tests.
+# See: https://docs.pytest.org/en/latest/pythonpath.html#pythonpath
--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import unittest
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().discover('tests', pattern='*.py')
-    unittest.TextTestRunner().run(suite)
