7b8121646e4c72d5c3e014c0e205e8e56810e330
Mark Spanbroek <mark@spanbroek.net>
2019-10-24 10:11:32 +0200

    Rename: active_rows -> included

--- src/dataset.py
+++ src/dataset.py
@@ -24,35 +24,35 @@ class Sample(Secret):
 @dataclass
 class ObliviousDataset(Secret):
     rows: [Sample]
-    active_rows: [Any]
+    included: [Any]
 
-    def __init__(self, *rows, active_rows=None):
+    def __init__(self, *rows, included=None):
         if len(rows) == 1 and isinstance(rows[0][0], Sample):
             self.rows = rows[0]
         else:
             self.rows = rows
-        self.active_rows = active_rows
+        self.included = included
 
     def column(self, index):
         number_of_columns = len(self.rows[0].inputs)
         is_selected = [i == index for i in range(number_of_columns)]
         values = mpc.matrix_prod([is_selected], self.rows, True)[0]
-        return ObliviousArray(*values, included=self.active_rows)
+        return ObliviousArray(*values, included=self.included)
 
-    def subset(self, active_rows):
+    def subset(self, included):
 
-        if isinstance(active_rows, ObliviousArray):
-            active_rows = list(active_rows.values)
+        if isinstance(included, ObliviousArray):
+            included = list(included.values)
 
-        subset_rows = active_rows
-        if self.active_rows:
-            subset_rows = mpc.schur_prod(subset_rows, self.active_rows)
-        return ObliviousDataset(self.rows, active_rows=subset_rows)
+        subset_rows = included
+        if self.included:
+            subset_rows = mpc.schur_prod(subset_rows, self.included)
+        return ObliviousDataset(self.rows, included=subset_rows)
 
     def is_active(self, row_index):
-        if self.active_rows == None:
+        if self.included == None:
             return s(1)
-        return self.active_rows[row_index]
+        return self.included[row_index]
 
     def __len__(self):
         return len(self.rows)
@@ -62,8 +62,8 @@ class ObliviousDataset(Secret):
 
     async def output(self):
         rows = [await output(row) for row in self.rows]
-        if self.active_rows:
-            active = await output(self.active_rows)
+        if self.included:
+            active = await output(self.included)
         else:
             active = [True] * len(rows)
         return [rows[i] for i in range(len(rows)) if active[i]]
