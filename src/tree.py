9412f59f36105a537b605cda4f25327dc8929105
Mark Spanbroek <mark@spanbroek.net>
2019-10-29 10:29:22 +0100

    Allow tree nodes to have branches

--- src/tree.py
+++ src/tree.py
@@ -6,9 +6,11 @@ from src.output import Secret, output
 @dataclass
 class Node(Secret):
     attribute: Any
-
-    def __init__(self, attribute):
-        self.attribute = attribute
+    left: Any = None
+    right: Any = None
 
     async def __output__(self):
-        return Node(await output(self.attribute))
+        attribute = await output(self.attribute)
+        left = await output(self.left) if self.left else self.left
+        right = await output(self.right) if self.right else self.right
+        return Node(attribute, left=left, right=right)
--- tests/tree.py
+++ tests/tree.py
@@ -8,3 +8,8 @@ from tests.reveal import reveal
 class TreeTest(unittest.TestCase):
     def test_reveal_single_node(self):
         self.assertEqual(reveal(Node(s(1))), Node(1))
+
+    def test_reveal_branches(self):
+        tree = Node(s(0), left=Node(s(1)), right=Node(s(2)))
+        expected_output = Node(0, left=Node(1), right=Node(2))
+        self.assertEqual(reveal(tree), expected_output)
