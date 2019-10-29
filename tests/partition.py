0b5f211d93e37890f6230be72c9d5e119ee8b01c
Mark Spanbroek <mark@spanbroek.net>
2019-10-29 10:46:26 +0100

    Change partition tests with examples that show continuous values

--- tests/partition.py
+++ tests/partition.py
@@ -13,36 +13,36 @@ def sample(*inputs):
 class PartitionTests(unittest.TestCase):
     def test_partition_on_continuous_attribute(self):
         data = ObliviousDataset(
-            sample(s(0), s(1), s(1), s(0)),
-            sample(s(1), s(0), s(1), s(1)),
-            sample(s(0), s(0), s(0), s(1))
+            sample(s(1), s(3), s(3), s(1)),
+            sample(s(3), s(1), s(3), s(3)),
+            sample(s(1), s(1), s(1), s(3))
         )
         left, right = partition_continuous(
-            data, attribute_index=s(2), threshold=s(0))
+            data, attribute_index=s(2), threshold=s(2))
         self.assertEqual(
             reveal(left),
             [
-                Sample([0, 0, 0, 1], 0)
+                Sample([1, 1, 1, 3], 0)
             ]
         )
         self.assertEqual(
             reveal(right),
             [
-                Sample([0, 1, 1, 0], 0),
-                Sample([1, 0, 1, 1], 0)
+                Sample([1, 3, 3, 1], 0),
+                Sample([3, 1, 3, 3], 0)
             ]
         )
 
     def test_partition_on_continuous_attribute_partial_dataset(self):
         data = ObliviousDataset(
-            sample(s(0), s(1), s(1), s(0)),
-            sample(s(1), s(0), s(1), s(1)),
-            sample(s(0), s(0), s(0), s(1))
+            sample(s(1), s(3), s(3), s(1)),
+            sample(s(3), s(1), s(3), s(3)),
+            sample(s(1), s(1), s(1), s(3))
         ).select([s(1), s(0), s(0)])
         left, right = partition_continuous(
-            data, attribute_index=s(2), threshold=s(0))
+            data, attribute_index=s(2), threshold=s(2))
         self.assertEqual(reveal(left), [])
-        self.assertEqual(reveal(right), [Sample([0, 1, 1, 0], 0)])
+        self.assertEqual(reveal(right), [Sample([1, 3, 3, 1], 0)])
 
     def test_partition_on_binary_attribute(self):
         data = ObliviousDataset(
