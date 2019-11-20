72081960292a5c5a6cd302548e2f5040c495f60e
Mark Spanbroek <mark@spanbroek.net>
2019-11-20 09:40:39 +0100

    Extract getitem()

--- src/classification.py
+++ src/classification.py
@@ -4,12 +4,14 @@ from mpyc.runtime import mpc
 
 def classify(sample, tree):
     if isinstance(tree, Branch):
-        attribute_index = tree.attribute
-        unit_vector = mpc.unit_vector(attribute_index, len(sample))
-        attribute_value = mpc.sum(mpc.schur_prod(sample, unit_vector))
         return mpc.if_else(
-            attribute_value,
+            getitem(sample, tree.attribute),
             classify(sample, tree.right),
             classify(sample, tree.left))
     if isinstance(tree, Leaf):
         return tree.outcome_class
+
+
+def getitem(sample, index):
+    unit_vector = mpc.unit_vector(index, len(sample))
+    return mpc.sum(mpc.schur_prod(sample, unit_vector))
