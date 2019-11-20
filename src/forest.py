d931f3fd791f94ea8067b829163f320b3532168e
Mark Spanbroek <mark@spanbroek.net>
2019-11-20 16:55:04 +0100

    Add random selection of attributes

--- src/forest.py
+++ src/forest.py
@@ -1,4 +1,6 @@
-from src.dataset import ObliviousDataset
+from mpyc.runtime import mpc
+from src.secint import secint
+from src.dataset import ObliviousDataset, Sample
 from src.train import train
 
 
@@ -8,3 +10,22 @@ def train_forest(samples, amount, depth):
 
 def bootstrap(samples):
     return ObliviousDataset([samples.choice() for _ in range(len(samples))])
+
+
+def random_attributes(samples, amount):
+    columns = random_columns(samples, amount)
+    outcomes = samples.outcomes
+    smaller_samples = []
+    for r in range(len(samples)):
+        inputs = []
+        outcome = outcomes[r]
+        for c in range(len(columns)):
+            inputs.append(columns[c][r])
+        smaller_samples.append(Sample(inputs, outcome))
+    return ObliviousDataset(smaller_samples)
+
+
+def random_columns(samples, amount):
+    indices = range(samples.number_of_attributes)
+    selected = mpc.random.sample(secint, indices, amount)
+    return [samples.column(index) for index in selected]
--- tests/forest.py
+++ tests/forest.py
@@ -1,7 +1,7 @@
 import unittest
 from src.dataset import ObliviousDataset
 from src.tree import Leaf, Branch
-from src.forest import train_forest, bootstrap
+from src.forest import train_forest, bootstrap, random_attributes
 from tests.example import samples
 from tests.reveal import reveal
 
@@ -33,9 +33,28 @@ class ForestTest(unittest.TestCase):
         for sample in selection:
             self.assertIn(sample, all)
 
+    def test_attribute_selection_is_random(self):
+        selection1 = reveal(random_attributes(samples, 3))
+        selection2 = reveal(random_attributes(samples, 3))
+        self.assertNotEqual(selection1, selection2)
+
+    def test_features_are_selected_from_the_original_sample(self):
+        selection = random_attributes(samples, 3)
+        selected_columns = reveal(columns(selection))
+        all_columns = reveal(columns(samples))
+        for column in selected_columns:
+            self.assertIn(column, all_columns)
+
 
 def depth(tree):
     if isinstance(tree, Leaf):
         return 0
     if isinstance(tree, Branch):
         return depth(tree.left) + 1
+
+
+def columns(samples):
+    return [
+        [samples[row][column] for row in range(len(samples))]
+        for column in range(samples.number_of_attributes)
+    ]
