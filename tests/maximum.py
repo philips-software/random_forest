fc0579d67c29797bacde019b93757d35067cf3c4
Mark Spanbroek <mark@spanbroek.net>
2019-12-05 12:48:09 +0100

    Simplify test

--- tests/maximum.py
+++ tests/maximum.py
@@ -5,20 +5,24 @@ from src.secint import secint as s
 from tests.reveal import reveal
 
 
+def q(numerator, denominator):
+    return s(numerator), s(denominator)
+
+
 class MaximumTest(unittest.TestCase):
 
     def test_maximum_with_two_arguments(self):
         self.assertEqual(
-            reveal(maximum((s(1), s(2)), (s(1), s(3)))), ((1, 2), 0))
+            reveal(maximum(q(1, 2), q(1, 3))), ((1, 2), 0))
         self.assertEqual(
-            reveal(maximum((s(1), s(4)), (s(1), s(3)))), ((1, 3), 1))
+            reveal(maximum(q(1, 4), q(1, 3))), ((1, 3), 1))
 
     def test_maximum_with_more_arguments(self):
         self.assertEqual(reveal(maximum(
-            (s(1), s(4)), (s(1), s(3)), (s(1), s(2)))), ((1, 2), 2))
+            q(1, 4), q(1, 3), q(1, 2))), ((1, 2), 2))
 
     def test_maximum_with_single_argument(self):
-        self.assertEqual(reveal(maximum((s(1), s(1)))), ((1, 1), 0))
+        self.assertEqual(reveal(maximum(q(1, 1))), ((1, 1), 0))
 
     def test_maximum_with_no_arguments(self):
         self.assertRaises(ValueError, maximum)
